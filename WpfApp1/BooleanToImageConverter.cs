759635f3a16c798848d12918a80922f7b0108233
leeky <hyperhand9@gmail.com>
2019-09-14 13:57:44 +0900

    wpf simple toggle image

---
+++ WpfApp1/BooleanToImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    class BooleanToImageConverter : IValueConverter
+    {
+        public BitmapImage On { get; set; }
+        public BitmapImage Off { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b = (bool)value;
+
+            return b ? On : Off;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp1/ImageToggleControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for ImageToggleControl.xaml
+    /// </summary>
+    public partial class ImageToggleControl : UserControl
+    {
+        public static DependencyProperty ImageToggleProperty = DependencyProperty.Register("ImageToggle", typeof(bool), typeof(ImageToggleControl), new PropertyMetadata(false));
+
+        public bool ImageToggle
+        {
+            get { return (bool)GetValue(ImageToggleProperty); }
+            set { SetValue(ImageToggleProperty, value); }
+        }
+
+        public ImageToggleControl()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+    }
+}
